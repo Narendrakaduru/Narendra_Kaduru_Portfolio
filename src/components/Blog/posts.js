f968b22cf55289773c6ae7d26b009f2f09cfd760
Nani <kadurunarendra@gmail.com>
2025-08-18 13:26:37 +0530

    update blog-image

--- src/components/Blog/posts.js
+++ src/components/Blog/posts.js
@@ -3,7 +3,7 @@ const blogs = [
     id: 1,
     slug: "getting-started-with-devops",
     title: "Getting Started with DevOps",
-    image: "../../../public/blogs/images/getting-started-with-devops.png",
+    image: "/blogs/images/getting-started-with-devops.png",
     date: "August 18, 2025",
     read_time: "4",
     excerpt: "Kicking off my DevOps blog series! Here’s a quick guide to the essential tools and technologies every DevOps enthusiast should know.",
